b4ac766a9b28e41bae91d20ae475fb388a17a506
joaocarloslima <joaocarloslima@me.com>
2025-08-21 09:39:50 -0300

    Add Lombok dependency and refactor Category model and CategoryController
    - Added Lombok dependency for simplified code.
    - Refactored Category model to use Lombok annotations for getters, setters, and constructor.
    - Updated CategoryController to manage categories with a repository and logging.

--- src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -2,23 +2,38 @@ package br.com.fiap.financewalk.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.financewalk.model.Category;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class CategoryController {
+
+    private List<Category> repository = new ArrayList<>();
 
     @GetMapping("/categories")
     public List<Category> index(){ 
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Lazer", "Popcorn"));
-        categories.add(new Category(2L, "Transporte", "Bus"));
-        categories.add(new Category(3L, "Educação", "Book"));
+        return repository;
+    }
 
-        return categories;
+    @PostMapping("/categories")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Category create(@RequestBody Category category){
+        category.setId(Math.abs(new Random().nextLong()));
+        log.info("criando categoria " + category);
+        repository.add(category);
+        return category;
     }
     
 }
