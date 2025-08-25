6bff3dfa3c4b311bf1472a2168877b03c3c5dd41
joaocarloslima <joaocarloslima@me.com>
2025-08-25 11:43:56 -0300

    Add delete category endpoint to CategoryController

--- src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
+++ src/main/java/br/com/fiap/financewalk/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,21 @@ public class CategoryController {
         if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
 
         return ResponseEntity.ok(categoryFound.get());
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Void> destroy(@PathVariable Long id){
+        log.info("apagando categoria com id {}", id);
+
+        var categoryFound = repository.stream()
+            .filter(category -> category.getId().equals(id))
+            .findFirst(); 
+        
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+
+        repository.remove(categoryFound.get());
+
+        return ResponseEntity.noContent().build();
     }
 
     
